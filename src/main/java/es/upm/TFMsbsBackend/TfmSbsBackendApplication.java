9286da0014f575a689ec4a5dee01a6d49acbb298
Sebastian <wangjiaxin6667@gmail.com>
2021-12-18 13:33:07 +0800

    Initial

---
+++ src/main/java/es/upm/TFMsbsBackend/TfmSbsBackendApplication.java
@@ -0,0 +1,13 @@
+package es.upm.TFMsbsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfmSbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfmSbsBackendApplication.class, args);
+	}
+
+}
