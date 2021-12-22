877d18bad6682b245f3d03609b2e072f89a26ffa
Sebastian <wangjiaxin6667@gmail.com>
2021-12-22 13:35:43 +0800

    Add start-up class

---
+++ service/service_edu/src/main/java/es/upm/tfm_sbs/service/edu/ServiceEduApplication.java
@@ -0,0 +1,13 @@
+package es.upm.tfm_sbs.service.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"es.upm.tfm_sbs"})
+public class ServiceEduApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEduApplication.class,args);
+    }
+}
