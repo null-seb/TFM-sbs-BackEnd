cb3d00ac134894bfafcd6a86e1c0b97848f7e69d
Sebastian <wangjiaxin6667@gmail.com>
2022-01-05 21:54:46 +0800

    Nacos

--- service/service_edu/src/main/java/es/upm/tfm_sbs/service/edu/ServiceEduApplication.java
+++ service/service_edu/src/main/java/es/upm/tfm_sbs/service/edu/ServiceEduApplication.java
@@ -2,10 +2,12 @@ package es.upm.tfm_sbs.service.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableDiscoveryClient
 @EnableOpenApi
 @SpringBootApplication
 @Import(value = {es.upm.tfm_sbs.service.base.handler.CommonMetaObjectHandler.class})
--- service/service_oss/src/main/java/es/upm/tfm_sbs/service/oss/ServiceOssApplication.java
+++ service/service_oss/src/main/java/es/upm/tfm_sbs/service/oss/ServiceOssApplication.java
@@ -3,10 +3,12 @@ package es.upm.tfm_sbs.service.oss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
 @ComponentScan({"es.upm.tfm_sbs"})
+@EnableDiscoveryClient
 public class ServiceOssApplication {
 
     public static void main(String[] args) {
