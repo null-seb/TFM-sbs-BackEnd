d3d1006141b401acf5fa367178da57c78f9190e5
Sebastian <wangjiaxin6667@gmail.com>
2022-01-16 20:23:10 +0800

    Adding timed tasks

--- service/service_statistics/src/main/java/es/upm/tfm_sbs/service/statistics/ServiceStatisticsApplication.java
+++ service/service_statistics/src/main/java/es/upm/tfm_sbs/service/statistics/ServiceStatisticsApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan({"es.upm.tfm_sbs"})
 @Import(value = {es.upm.tfm_sbs.service.base.handler.CommonMetaObjectHandler.class})
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableScheduling
 public class ServiceStatisticsApplication {
 
     public static void main(String[] args) {
